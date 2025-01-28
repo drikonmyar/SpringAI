306043cbad3b3f4e141e2e3a42831849311eb307
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-01-29 01:34:05 +0530

    added open ai

--- src/main/java/com/ai/OllamaController.java
+++ src/main/java/com/ai/OllamaController.java
@@ -1,12 +1,6 @@
 package com.ai;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.MessageType;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.ai.ollama.api.OllamaModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,21 +8,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.util.Map;
-
 @RestController
-@RequestMapping("/ai")
-public class AIController {
+@RequestMapping("/ollama")
+public class OllamaController {
 
     @Autowired
     private OllamaChatModel ollamaChatModel;
 
-    @GetMapping("/ollama")
+    @GetMapping("/response")
     public String getResponse(@RequestParam("prompt") String prompt){
         return ollamaChatModel.call(prompt);
     }
 
-    @GetMapping("/ollama/stream")
+    @GetMapping("/response/stream")
     public Flux<String> getResponseInStream(@RequestParam("prompt") String prompt){
         return ollamaChatModel.stream(prompt);
     }
